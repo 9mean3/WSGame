ef00c3e56e90f1a190eec72431ea4d450e8d937e
9mean3 <gminseong114@gmail.com>
2023-09-25 20:30:12 +0900

    d

--- Assets/ShoulderViewCinemachine.cs
+++ Assets/ShoulderViewCinemachine.cs
@@ -26,22 +26,27 @@ public class ShoulderViewCinemachine : MonoBehaviour
         dir.x = Mathf.Clamp(dir.x, -80, 80);
         target.localEulerAngles = dir;
 
+    }
+
+    private void FixedUpdate()
+    {
         CKObstacle();
+        Debug.Log(thisCam.GetCinemachineComponent<CinemachineTransposer>().m_FollowOffset.z);
     }
 
     void CKObstacle()
     {
-        Ray playertocamRay = new Ray(target.position, -target.forward);
         float fwd = thisCam.GetCinemachineComponent<CinemachineTransposer>().m_FollowOffset.z;
 
-        if (Physics.Raycast(playertocamRay, out RaycastHit hit, fwd, whatIsObstacle)){
+        if (Physics.Raycast(target.position, -target.forward, out RaycastHit hit, -fwd, whatIsObstacle)){
             thisCam.GetCinemachineComponent<CinemachineTransposer>().m_FollowOffset.z
-                = Vector3.Distance(target.position, hit.point);
-            print(thisCam.GetCinemachineComponent<CinemachineTransposer>().m_FollowOffset.z);
+                = -Vector3.Distance(target.position, hit.point);
+            Debug.Log("¿Ô¾î~!");
         }
-        //else
-        //{
-        //    thisCam.GetCinemachineComponent<CinemachineTransposer>().m_FollowOffset.z = followDistance;
-        //}
+        else if(Physics.Raycast(target.position, -target.forward, followDistance, whatIsObstacle))
+        {
+            Debug.Log("³ª°¬¾î~!");
+            thisCam.GetCinemachineComponent<CinemachineTransposer>().m_FollowOffset.z = followDistance;
+        }
     }
 }
