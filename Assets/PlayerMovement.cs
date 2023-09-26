ef72e7720d239425e8f3121f3f2acfcfa4018630
9mean3 <gminseong114@gmail.com>
2023-09-27 01:49:55 +0900

    ah shit

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] Transform camPivot;
+    [SerializeField] Transform orientation;
+    [SerializeField] Transform player;
+
+    [SerializeField] float rotationSpeed;
 
     private void Awake()
     {
@@ -17,9 +20,13 @@ public class PlayerMovement : MonoBehaviour
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
-        if(Mathf.Abs(h) > 0.1f || Mathf.Abs(v) > 0.1f)
+        Vector3 viewDir = new Vector3(transform.position.x, player.position.y, transform.position.z);
+        orientation.forward = viewDir.normalized;
+
+        Vector3 inputDir = orientation.forward * v + orientation.right * h;
+        if(inputDir != Vector3.zero)
         {
-            
+            player.forward = Vector3.Slerp(player.forward, inputDir, rotationSpeed * Time.deltaTime);
         }
     }
 }
